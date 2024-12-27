42bd5f10f92f953f3eafb14eb391d48b521d2ff7
ShreyashSomvanshi <shreyash.somvanshi.aids.2020@vpkbiet.org>
2024-12-27 15:56:28 +0530

    testing cicd, added form

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
-import numpy as numpy
+import numpy as np
 
 app = Flask(__name__)
 model = joblib.load('rf_model.joblib')
 
 @app.route('/', methods=['GET'])
 def home():
-	return "Hello World!"
-	# return {'message':'Iris Model API'}
-
+	return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-	data = request.get_json(force=True)
-	prediction = model.predict(np.array(data['features']).reshape(1,-1))
-	return jsonify({'prediction': prediction[0]})
+	data = request.form
+	features = [float(data['sepal_length']),
+				float(data['sepal_width']),
+				float(data['petal_length']),
+				float(data['petal_width'])]
+	prediction = model.predict(np.array(features).reshape(1,-1))
+	return render_template('index.html', prediction = prediction[0])
 
 if __name__ == "__main__":
 	app.run(debug=True)
 
+# rnd_czX5LhGAjoa0xrRGSmQ8SwCcssQi
